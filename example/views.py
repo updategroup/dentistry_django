bbcf0eabd9743c470adb6d269d4b194db5d2564f
tranquoc113 <tranquoc631@gmail.com>
2023-11-23 18:15:29 +0700

    test db

--- example/views.py
+++ example/views.py
@@ -1,15 +1,18 @@
 # example/views.py
 # from datetime import datetime
-
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
+User = get_user_model()
+
 def index(request):
+    print(User.objects.all())
     # now = datetime.now()
     html = f'''
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
-            <p>The current time is.</p>
+            <p>The current time is. {'Khong thay User' if User.objects.exists() else 'Not Found'}</p>
         </body>
     </html>
     '''
