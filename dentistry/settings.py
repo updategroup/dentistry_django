1443aeaf9c2950026f3dcf8f532c494a2e660363
tranquoc113 <tranquoc631@gmail.com>
2023-11-23 21:09:20 +0700

    update

--- dentistry/settings.py
+++ dentistry/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 import os
 
