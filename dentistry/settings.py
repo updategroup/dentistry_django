2503314fcd53491f4c0832f47c2da21cbfd5dd39
tranquoc113 <tranquoc631@gmail.com>
2023-11-23 20:34:34 +0700

    update test db

--- dentistry/settings.py
+++ dentistry/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# from dotenv import load_dotenv
+# load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,10 +78,15 @@ WSGI_APPLICATION = 'dentistry.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'E4QNkoASX6eQZe6B',
+        'HOST': 'db.cngnishlcaemwvwgykwj.supabase.co',
+        'PORT': '5432',
     }
 }
+
 # DATABASES = {}
 
 
